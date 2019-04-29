dd21042fa152406f8f8cb46835bf11c815ea5d09
ChrisYeomans <chrisj.yeomans@gmail.com>
2019-04-29 20:02:46 +0100

    Rewrote the price getting code in a different api as the old one is deprecated, also added some new functions, notabley !list and !supported

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 #! usr/bin/env python3
 import discord, dbl, asyncio
-from crypto_price_scrape import get_price
+from crypto_price_scrape import get_price, get_crypto_list
 
 def main():
 	TOKEN = "NTcxNDk3MTU0Nzc5MTUyMzg0.XMOtTw.6FfMMiNuvIws67IpFNMKgf_pJaU"
@@ -12,14 +12,21 @@ def main():
 	
 	@client.event
 	async def on_message(message):
-		if message.content[:6] == "!price":
+		if message.content[:6].lower() == "!price":
 			curr = message.content[7:]
 			print(curr)
 			price = get_price(curr.upper())
 			if price:
-				await message.channel.send("One "+curr.upper()+" is worth €"+str(price))
+				await message.channel.send("One "+curr.upper()+" is worth $"+str(price))
 			else:
 				await message.channel.send("Sorry that is not a recognised currency")
+		if message.content[:5].lower() == "!list":
+			await message.channel.send("The following currencies are supported: "+', '.join(get_crypto_list()))
+		if message.content[:10].lower() == "!supported":
+			if message.content[11:].upper() in get_crypto_list():
+				await message.channel.send("That currency is supported")
+			else:
+				await message.channel.send("That currency is not supported")
 
 	client.run(TOKEN)
 
