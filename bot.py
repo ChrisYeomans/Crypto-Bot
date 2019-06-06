466ba02a0bd06f011f3eff75766c9845dc8cd0d6
ChrisYeomans <chrisj.yeomans@gmail.com>
2019-06-06 05:24:33 +0100

    Cleaned up some things and fixed token location.

--- bot.py
+++ bot.py
@@ -2,8 +2,16 @@
 import discord, dbl, asyncio
 from crypto_price_scrape import get_price, get_crypto_list
 
+HELP_MESSAGE = """
+**This bot does all things crypto the commands are as follows:**\n
+!price currency - Gives the current price of your desired currency name, use ticker codes like BTC or ETH
+!list - Gives a list of currently supported Crypto-Currencies
+!supported currency - Says whether or not a specific currenty is supported
+!? currency - See !supported
+"""
+
 def main():
-	TOKEN = "NTcxNDk3MTU0Nzc5MTUyMzg0.XMOtTw.6FfMMiNuvIws67IpFNMKgf_pJaU"
+	TOKEN = input()
 	client = discord.Client()
 
 	@client.event
@@ -20,13 +28,21 @@ def main():
 				await message.channel.send("One "+curr.upper()+" is worth $"+str(price))
 			else:
 				await message.channel.send("Sorry that is not a recognised currency")
-		if message.content[:5].lower() == "!list":
+		elif message.content[:5].lower() == "!list":
 			await message.channel.send("The following currencies are supported: "+', '.join(get_crypto_list()))
-		if message.content[:10].lower() == "!supported":
+		elif message.content[:10].lower() == "!supported":
 			if message.content[11:].upper() in get_crypto_list():
+				await message.channel.send("That currency *is* supported")
+			else:
+				await message.channel.send("That currency *is not* supported")
+		elif message.content[:2] == '!?':
+			if message.content[3:].upper() in get_crypto_list():
 				await message.channel.send("That currency is supported")
 			else:
 				await message.channel.send("That currency is not supported")
+		elif message.content.lower() == "!help" or message.content.lower() == "!h":
+			await message.channel.send(HELP_MESSAGE)
+
 
 	client.run(TOKEN)
 
