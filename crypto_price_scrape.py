deabb371820893f2b7f0e0ea5245754967be7f87
ChrisYeomans <chrisj.yeomans@gmail.com>
2020-05-10 17:57:35 +0000

    rewrote scraping in new api

--- crypto_price_scrape.py
+++ crypto_price_scrape.py
@@ -1,20 +1,17 @@
 #! usr/bin/env python3
-import ccxt
-
-bittrex = ccxt.bittrex()
+import cryptocompare
 
 def main():
 	print(get_crypto_list())
 	print(get_price("BTC"))
-	print(printable_crypto_list())
 
 def get_price(crypto):
 	if crypto.upper() in get_crypto_list():
-		return ccxt.coinmarketcap().fetch_ticker(crypto.upper()+"/USD")['info']['price_usd']
+		return cryptocompare.get_price('BTC',curr='USD',full=True)['RAW'][crypto.upper()]['USD']['PRICE']
 	return ""
 
 def get_crypto_list():
-	return {s.split('/')[0] for s in ccxt.coinmarketcap().fetch_tickers()}
+	return [e for e in cryptocompare.get_coin_list(format=False)]
 
 if __name__ == "__main__":
     main()
